f112b2c9171b90f8c6c739c5e09f7085cf0e205e
madhwalshivam <madhwalshivam08@gmail.com>
2025-04-26 13:12:25 +0530

    Your update message here

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -263,9 +263,9 @@ const LoginPage = ({ role }) => {
                     sx={{
                         backgroundImage: `url(${bgpic})`,
                         backgroundRepeat: 'no-repeat',
+                        backgroundSize: '80%',
                         backgroundColor: (t) =>
                             t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-                        backgroundSize: 'cover',
                         backgroundPosition: 'center',
                     }}
                 />
